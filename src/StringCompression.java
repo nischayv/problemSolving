56f61dbc894c576e6823753e21a34d00189c079a
Nischay Venkatram <nischay.v@gmail.com>
2016-10-09 22:40:48 -0500

    String compression problem

---
+++ src/StringCompression.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+/**
+ * Cracking the coding interview String Compression
+ */
+public class StringCompression {
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.next();
+        StringBuilder finalString = new StringBuilder();
+        int count = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            count++;
+
+            if((i+1) == s.length() || s.charAt(i) != s.charAt(i+1)) {
+                finalString.append(s.charAt(i));
+                finalString.append(count);
+                count = 0;
+            }
+        }
+
+        if(finalString.length() < s.length()) {
+            System.out.println(finalString);
+        }
+        else {
+            System.out.println(s);
+        }
+    }
+}
