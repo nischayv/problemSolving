e361f3d65a1816b641d5f5350679ad1d6db5de66
Nischay Venkatram <nischay.v@gmail.com>
2016-10-10 15:26:23 -0500

    Finished kth to last

--- src/KthToLast.java
+++ src/KthToLast.java
@@ -3,7 +3,7 @@
  */
 public class KthToLast {
 
-    private class Node {
+    public static class Node {
         int data;
         Node next;
 
@@ -13,14 +13,21 @@ public class KthToLast {
         }
     }
 
+    public static void main(String args[]) {
+        Node t = new Node(2, null);
+        Node n = new Node(1, t);
+        t.next = new Node(3, new Node(4, null));
+        printKthToLast(n, 1);
+    }
+
     private static int printKthToLast(Node head, int k){
         if(head == null) {
-            return -1;
+            return 0;
         }
 
         int index = printKthToLast(head.next, k) + 1;
         if(index == k) {
-            System.out.println("Kth to the last node is" + head.data);
+            System.out.println("Kth to the last node is " + head.data);
         }
         return index;
     }
