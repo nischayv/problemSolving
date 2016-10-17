506e03377c3fc68d61b04c3fe190d116d0735627
Nischay Venkatram <nischay.v@gmail.com>
2016-10-17 14:19:21 -0500

    Finished the power set problem

--- src/Recursion/PowerSet.java
+++ src/Recursion/PowerSet.java
@@ -8,8 +8,40 @@ import java.util.ArrayList;
  */
 public class PowerSet {
 
-    private ArrayList<ArrayList<Integer>> powerset(ArrayList<Integer> set, int inderx) {
+    private static ArrayList<ArrayList<Integer>> powerset(ArrayList<Integer> set, int index) {
+        ArrayList<ArrayList<Integer>> allsubsets;
+        if(set.size() == index) {
+            allsubsets = new ArrayList<ArrayList<Integer>>();
+            allsubsets.add(new ArrayList<Integer>());
+        }
+        else {
+            allsubsets = powerset(set, index + 1);
+            int item = set.get(index);
+            ArrayList<ArrayList<Integer>> moresubsets = new ArrayList<ArrayList<Integer>>();
+            for(ArrayList<Integer> subset : allsubsets) {
+                ArrayList<Integer> newsubset = new ArrayList<Integer>();
+                newsubset.addAll(subset);
+                newsubset.add(item);
+                moresubsets.add(newsubset);
+            }
+            allsubsets.addAll(moresubsets);
+        }
+        return allsubsets;
+    }
 
+    public static void main(String args[]) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        ArrayList<ArrayList<Integer>> subsets;
+        subsets = powerset(arrayList, 0);
 
+        for(ArrayList<Integer> subset: subsets) {
+            for(Integer item: subset) {
+                System.out.print(item + " ");
+            }
+            System.out.println("");
+        }
     }
 }
