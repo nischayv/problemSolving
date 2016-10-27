3b9c06b08fd6d285c1a43b39e7a08e2bd13b74a1
Nischay Venkatram <nischay.v@gmail.com>
2016-10-27 15:24:23 -0500

    Tried the At index I interview question. Works fine.

---
+++ src/ArraysAndLists/AtIndexI.java
@@ -0,0 +1,30 @@
+package ArraysAndLists;
+
+/**
+ * Given an array with fixed length, check if each element is the same as the index
+ * Tried a binary search type approach. May not be that effective since we have to iterate
+ * through the entire array regardless. Unless we just want to know of there is any index that
+ * is incorrect and not the first incorrect.
+ */
+public class AtIndexI {
+    public static void main(String args[]) {
+        int[] arr = {1, 2, 3};
+        System.out.println(find(arr, 0, arr.length - 1));
+    }
+
+    private static int find(int arr[], int start, int end) {
+        if(start > end) {
+            return -1;
+        }
+        int mid = (start + end) / 2;
+        int left = find(arr, start, mid - 1);
+        if(left != -1) {
+            return left;
+        }
+        if(mid != arr[mid]) {
+            return mid;
+        }
+        return find(arr, mid + 1, end);
+    }
+
+}
