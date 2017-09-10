62453e1daee3df2107652fe20c921c36883c8539
Nischay Venkatram <nischay.v@gmail.com>
2017-09-09 19:34:15 -0500

    add list cycle detection method

--- src/ArraysAndLists/LinkedListCycle.java
+++ src/ArraysAndLists/LinkedListCycle.java
@@ -32,4 +32,34 @@ public class LinkedListCycle {
         }
         return null;
     }
+
+    ListNode getCycleStart (ListNode start) {
+        if (start == null) {
+            return null;
+        }
+
+        ListNode n1 = start;
+        ListNode n2 = start;
+
+        while (n2 != null && n2.next != null) {
+            n1 = n1.next;
+            n2 = n2.next.next;
+            if (n1 == n2) {
+                break;
+            }
+        }
+
+        if (n2 == null || n2.next == null) {
+            return null;
+        }
+
+        n1 = start;
+
+        while (n1 != n2) {
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+
+        return n1;
+    }
 }
