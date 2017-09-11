1746f6182e1a0e4e86d1dd6d9c082b488bdfe806
Nischay Venkatram <nischay.v@gmail.com>
2017-09-11 01:20:26 -0500

    add list intersection algorithm

---
+++ src/StacksAndQueues/StackMin.java
@@ -0,0 +1,41 @@
+package StacksAndQueues;
+
+import java.util.Stack;
+
+public class StackMin extends Stack<Integer> {
+    private Stack<Integer> s2;
+
+    public StackMin () {
+        s2 = new Stack<Integer>();
+    }
+
+    public void push(int value) {
+        if (value < min()) {
+            s2.push(value);
+        }
+        super.push(value);
+    }
+
+    @Override
+    public Integer pop () {
+        int value = super.pop();
+        if (value == min()) {
+            s2.pop();
+        }
+        return value;
+    }
+
+
+    public int min () {
+        if (s2.isEmpty()) {
+            return Integer.MAX_VALUE;
+        } else {
+            return s2.peek();
+        }
+    }
+
+}
+
+
+
+
