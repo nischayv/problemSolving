dafdddc7297cdd09a63fa7cf570893e5c2e2d23e
Nischay Venkatram <nischay.v@gmail.com>
2017-09-21 01:03:14 -0500

    coins prob using dp

--- src/DynamicProgramming/Coins.java
+++ src/DynamicProgramming/Coins.java
@@ -1,5 +1,7 @@
 package DynamicProgramming;
 
+import java.util.Hashtable;
+
 public class Coins {
 
     public static void main(String args[]) {
@@ -25,6 +27,37 @@ public class Coins {
             int moneyRemaining = amount - i * denomination;
             ways += findWays (moneyRemaining, money, index+1);
         }
+        return ways;
+    }
+
+    public static int makeChangeDp () {
+        Hashtable<String, Integer> ht = new Hashtable<String, Integer>();
+        return findWays(1423432285, new int[]{25,10,5,1}, 0, ht);
+    }
+
+    public static int findWays (int amount, int[] money, int index, Hashtable<String, Integer> ht) {
+
+        if (amount == 0) {
+            return 1;
+        }
+        if (index >= money.length) {
+            return 0;
+        }
+        String key = amount + "-" + index;
+
+        if (ht.containsKey(key)){
+            return ht.get(key);
+        }
+
+
+        int ways = 0;
+        int denomination = money[index];
+
+        for (int i = 0; i * denomination <= amount; i++) {
+            int moneyRemaining = amount - i * denomination;
+            ways += findWays(moneyRemaining, money, index+1, ht);
+        }
+        ht.put(key, ways);
         return ways;
     }
 }
